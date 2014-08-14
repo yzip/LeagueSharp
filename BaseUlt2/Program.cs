6be8bcf5edddd60061f312d900a254d16b06a69e
Beaving <beaving@gmail.com>
2014-08-14 14:14:13 +0200

    First commit

---
+++ BaseUlt2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseUlt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

b34b4b0e0743d72cbb5216a1b9ad01840987eb27
Beaving <beaving@gmail.com>
2014-10-11 05:07:31 +0200

    - Revived KarthusSharp

--- KarthusSharp/Program.cs
+++ KarthusSharp/Program.cs
@@ -1,32 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-
 using LeagueSharp;
 using LeagueSharp.Common;
-using SharpDX;
 
 namespace KarthusSharp
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Game.OnGameStart += Game_OnGameStart;
+        public static Helper Helper;
 
-            if (Game.Mode == GameMode.Running)
-                Game_OnGameStart(new EventArgs());
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
         }
 
-        private static void Game_OnGameStart(EventArgs args)
+        private static void Game_OnGameLoad(EventArgs args)
         {
-            if (ObjectManager.Player.ChampionName != "Karthus")
-                return;
-
-            Game.PrintChat("KarthusSharp: Not yet keppo");
+            Helper = new Helper();
+            new Karthus();
         }
     }
 }

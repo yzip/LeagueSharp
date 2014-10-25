2966d71fb3339eefeca427395c0e83b22b8adeab
Beaving <beaving@gmail.com>
2014-10-25 17:08:41 +0200

    Test

--- KarthusSharp/Karthus.cs
+++ KarthusSharp/Karthus.cs
@@ -111,11 +111,8 @@ namespace KarthusSharp
             switch (_orbwalker.ActiveMode)
             {
                 case Orbwalking.OrbwalkingMode.Combo:
-                    //_orbwalker.SetAttack(_menu.Item("comboAA").GetValue<bool>() || ObjectManager.Player.Mana < 100); //if no mana, allow auto attacks!
-
-                    //Game.PrintChat("k: " + (_menu.Item("comboAA").GetValue<bool>() || ObjectManager.Player.Mana < 100));
-
-                    //_orbwalker.SetMovement(_menu.Item("comboMove").GetValue<bool>());
+                    _orbwalker.SetAttack(_menu.Item("comboAA").GetValue<bool>() || ObjectManager.Player.Mana < 100); //if no mana, allow auto attacks!
+                    _orbwalker.SetMovement(_menu.Item("comboMove").GetValue<bool>());
                     Combo();
                     break;
                 case Orbwalking.OrbwalkingMode.Mixed:
